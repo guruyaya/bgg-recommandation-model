cd0481739f8d4dbecdfd0415a5bce54889865b54
guruyaya <guruyaya@gmail.com>
2021-04-04 07:04:36 +0000

    fixed shuffling

--- shuffle_15m_comments.py
+++ shuffle_15m_comments.py
@@ -15,6 +15,13 @@ df = df.sample(frac=1, random_state=42).reset_index(drop=True)
 print ("Reordered")
 print (df.head())
 
-print ("Number of reviews:", len(df))	
+num_reviews = len(df)
+print ("Number of reviews:", num_reviews)	
 
-# df.to_feather('processed/train/bgg-reviews.feather', index=True)
+max_train = num_reviews * 6 // 10
+max_val = num_reviews * 8 // 10
+print ("Splitting")
+df.iloc[:max_train].to_feather('processed/bgg-reviews_train.feather')
+df.iloc[max_train:max_val].reset_index().to_feather('processed/bgg-reviews_val.feather')
+df.iloc[max_val:].reset_index().to_feather('processed/bgg-reviews_val.feather')
+print ("Done")
