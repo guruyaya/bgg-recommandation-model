3572c330b1f56b6ab1cfe9c46e9a04995668595a
guruyaya <guruyaya@gmail.com>
2021-04-05 02:32:51 +0000

    create seprate files

---
+++ create_user_grouped_files.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import pandas as pd
+import numpy as np
+import sys
+
+def split_file(file_name, target_lib, lines=100_000):
+    print ("Starting")
+    user_df = pd.read_feather(file_name)
+    user_df = user_df[ ~user_df['user'].isna() ]
+    user_df['user'] = user_df['user'].str.strip()
+    print ("Loaded")
+    user_df = user_df.sort_values(['user']).reset_index()
+    print ("Sorted")
+    max_loc_per_user = user_df.groupby('user').apply(lambda df: df.index.max())
+    print ("Grouped")
+
+    df_from = 0
+    while True:
+        ref_num = df_from + 100_000
+        df_to = max_loc_per_user[ max_loc_per_user >= ref_num].min()
+
+        if df_to == np.nan:
+            save_df = user_df.loc[df_from:]
+        else:
+            save_df = user_df.loc[df_from:(df_to + 1)]
+
+        if len(save_df) == 0:
+            break
+
+        print (f"Saving from {df_from} to {df_to}")
+        print ("starting user: {}, ending user: {}".format(save_df.iloc[0].user, save_df.iloc[-1].user))
+        r = save_df.reset_index(drop=True)
+        r.to_feather(f'{target_lib}/reviews_from_{df_from}_to_{df_to}.feather')
+        df_from = df_to
+    print ("Done")
+
+if __name__ == '__main__':
+    print ("Train")
+    split_file('processed/bgg-reviews_train.feather','processed/train')
+    print ("Val")
+    split_file('processed/bgg-reviews_val.feather','processed/val', 300_000)
+    print ("Test")
+    split_file('processed/bgg-reviews_test.feather','processed/test', 300_000)
